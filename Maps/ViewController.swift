96e0885e6341696662ee16d42d2e68aa7ddf1c01
HoodsDream <idinkyy@gmail.com>
2015-04-03 15:57:06 -0700

    first commit

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -7,12 +7,63 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var latitude:CLLocationDegrees = 35.6833
+        var longitude:CLLocationDegrees = 139.6833
+        
+        var latDelta:CLLocationDegrees = 0.01
+        var lonDelta:CLLocationDegrees = 0.01
+        
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location,span)
+        
+        map.setRegion(region, animated:true)
+        
+        var annotation = MKPointAnnotation()
+        
+        annotation.coordinate = location
+        
+        annotation.title = "Tokyo hommie!"
+        
+        annotation.subtitle = "Someday i'll go here"
+        
+        map.addAnnotation(annotation)
+        
+        var longPressRecognizer = UILongPressGestureRecognizer(target: self, action: "action:")
+        
+        longPressRecognizer.minimumPressDuration = 2
+        
+        map.addGestureRecognizer(longPressRecognizer)
+        
+    }
+    
+    
+    func action(gestureRecognizer: UIGestureRecognizer) {
+        
+        var touchPoint = gestureRecognizer.locationInView(self.map)
+        
+        var newCoordinate: CLLocationCoordinate2D = map.convertPoint(touchPoint, toCoordinateFromView:self.map)
+        
+        var annotation = MKPointAnnotation()
+        
+        annotation.coordinate = newCoordinate
+        
+        annotation.title = "New Spot"
+        
+        annotation.subtitle = "Someday i'll go here"
+
+        map.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +71,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
